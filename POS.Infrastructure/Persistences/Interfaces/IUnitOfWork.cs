b5a3e872b6b45029d496f313146f099ee0d28ce2
AlainGS <i201921039@cibertec.edu.pe>
2023-11-18 22:57:44 -0500

    Agregar archivos de proyecto.

---
+++ POS.Infrastructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Declaracion o matricula de nuestra interface a nivel de repository
+        ICategoryRepository Category {  get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
